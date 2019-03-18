162ca408e9dc2e74ce3c2e465c4e9e53c289bc5a
Michelle-lele <fia_m@abv.bg>
2019-03-18 22:03:06 +0200

    urls changed, added index route alias, added dummy user greeting

--- application.py
+++ application.py
@@ -4,8 +4,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index.html")
 def index():
-	return render_template("index.html")
+	username = "genadi"
+	username = username.capitalize()
+	return render_template("index.html", username=username)
 
 @app.route("/template-leia.html")
 def template_leia():
