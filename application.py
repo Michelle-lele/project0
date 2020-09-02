9f03f375a30c93ee862c449b05b097a021f6612a
Michelle-lele <fia_m@abv.bg>
2020-09-02 17:29:45 +0300

    Added Postgress db, organised site templates structure

--- application.py
+++ application.py
@@ -1,20 +1,73 @@
-from flask import Flask, render_template, request, session
+import os 
 
+from flask import Flask, render_template, request, session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 app.config.from_envvar('APP_SETTINGS')
 
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+	raise RuntimeError("DATABASE_URL is not set")
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
 @app.route("/")
 @app.route("/index.html")
 def index():
-	user_name = "genadi"
+	user_name = ""
 	user_name = user_name.capitalize()
 	return render_template("index.html", user_name=user_name)
 
 @app.route("/signup.html", methods=["POST", "GET"])
 def signup():
-	user_email = request.form.get("email")
-	return render_template("signup.html", user_email=user_email)
+	if request.method == "POST":
+		errorMessages= []
+		#ensure required fields are submitted
+		if not request.form.get("username"):
+			errorMessages.append("Username is required!")
+		
+		if not request.form.get("password"):
+			errorMessages.append("Password is required!")
+
+		if not request.form.get("confirm_password"):
+			errorMessages.append("Confirm Password is required!")
+
+		if errorMessages != []:
+			return render_template("signup.html", errorMessages=errorMessages)
+
+		#ensure username has no spaces
+		if ' ' in request.form.get("username"):
+			errorMessages.append("Username should not contain spaces!")
+
+		#ensure passwords match
+		if request.form.get("password") != request.form.get("confirm_password"):
+			errorMessages.append("Passwords don't match!")
+
+		if errorMessages != []:
+			return render_template("signup.html", errorMessages=errorMessages)
+
+		#ensure password has minimum 8 characters, including characters & numbers
+		if len(request.form.get("password")) < 8:
+			errorMessages.append("Password should be at least 8 symbols!")
+		if not request.form.get("password").isalnum():
+			errorMessages.append("Password should contain at least 1 letter or digit!")
+
+		if errorMessages != []:
+			return render_template("signup.html", errorMessages=errorMessages)
+
+		#ensure username doesn't exist
+		# username= db.execute("SELECT * FROM users WHERE username = :username", {"username": request.form.get("username")}).fetchall()
+		# #print(usernames, file=sys.stderr)
+		# if len(username) != 0:
+		# 	errorMessages.append("Username already exists!")
+		# 	return render_template("signup.html", errorMessages=errorMessages)
+
+	return render_template("signup.html")
 
 
 @app.route("/template-leia.html")
