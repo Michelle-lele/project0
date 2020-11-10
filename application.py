6357e285bc5280057e627b8069e8883bae167169
Michelle-lele <fia_m@abv.bg>
2020-11-10 18:19:31 +0200

    Not working version of login required.

--- application.py
+++ application.py
@@ -1,23 +1,42 @@
 import os, sys
 
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import (
+	g,
+	Flask,
+	redirect,
+	render_template,
+	request,
+	session,
+	url_for
+	)
+from flask_session import Session
 from functools import wraps
 from werkzeug.security import check_password_hash, generate_password_hash
 from database import db
 from models import User
+from datetime import timedelta
 
 app = Flask(__name__)
 app.config.from_envvar('APP_SETTINGS')
 
-#def login_required(f):
+app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=10)
+Session(app)
 
+def login_required(f):
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		if user_id not in session:
+			return redirect(url_for('login', next=request.url))
+		return f(*args, **kwargs)
+	return decorated_function
 
+@login_required
 @app.route("/")
 @app.route("/index.html")
 def index():
-	user_name = ""
-	user_name = user_name.capitalize()
-	return render_template("index.html", user_name=user_name)
+	return render_template("index.html")
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
@@ -76,6 +95,9 @@ def signup():
 def login():
 	errorMessages = []
 
+	if 'user_id' in session:
+		return redirect(url_for('index'))
+
 	if request.method == "POST":
 		if not request.form.get("email"):
 			errorMessages.append("Username is required!")
@@ -86,19 +108,24 @@ def login():
 		if errorMessages != []:
 			return render_template("login.html", errorMessages=errorMessages)
 
-		user = db.query(User).filter(User.email == request.form.get("email")).count()
-		#WIP check hash
-		password = db.query(User).filter(User.password == request.form.get("password")).count()
-		print(user, file=sys.stderr)
-		print(password, file=sys.stderr)
+		user_count = db.query(User).filter(User.email == request.form.get("email")).count()
+		user = db.query(User).filter(User.email == request.form.get("email"))
 		
-		if user == 1 or password == 1:
+		if user_count != 1 or not check_password_hash(user[0].password, request.form.get("password")):
 			errorMessages.append("Your username or password are not correct!")		
 			return render_template("login.html", errorMessages=errorMessages)
 		else:
+			session['user_id'] = user[0].id
+			#g.user = user[0]
 			return redirect(url_for('index'))
 
 	return render_template("login.html")
+
+@app.route("/logout", methods = ['POST', 'GET'])
+def logout():
+	session.clear()
+	#g.user = None
+	return redirect(url_for("login"))
 
 @app.route("/template-leia.html")
 def template_leia():
